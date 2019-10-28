4102933f4af8647d78a55d557732d41ee972713a
Romulo-Reis <romuloreistavares@hotmail.com>
2019-10-27 22:56:19 -0300

    Altaração para o calculo correto do fatorial de zero

--- Fatorial.java
+++ Fatorial.java
@@ -5,8 +5,11 @@ public class Fatorial{
 		if(n < 0){
 			throw new IllegalArgumentException("Não existe fatorial para números negativos.");
 		}
-		for(int i = 2; i <= n; i++){
-			resultado*=i;
+		
+		if(n != 0){
+			for(int i = 2; i <= n; i++){
+				resultado*=i;
+			}
 		}
 		return resultado;
 	}
